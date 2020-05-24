c07d4c78eeb63022a9f38a5ff55a8f8705e10904
Ferenc Besenyei <55558183+fepu08@users.noreply.github.com>
2020-05-24 10:44:55 +0200

    created and edited tables
    likes, like_types,  main_categories, sub_categories ...

--- codeigniter-forum.sql
+++ codeigniter-forum.sql
@@ -5,12 +5,51 @@ MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USE
 GRANT ALL PRIVILEGES ON `codeigniter-forum`.* TO 'codeigniter-forum-user'@'localhost';
 
 
-
+-- Create posts table
 CREATE TABLE `codeigniter-forum`.`posts` (
      `id` INT NOT NULL AUTO_INCREMENT , 
      `user_id` INT NOT NULL , 
-     `category_id` INT NOT NULL , 
-     `title` VARCHAR(250) NOT NULL ,
+     `subcategory_id` INT NOT NULL , 
+     `parent_post_id` INT NOT NULL DEFAULT '0' ,
      `body` TEXT NOT NULL , 
      `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
-     PRIMARY KEY (`id`)) ENGINE = InnoDB;
+     PRIMARY KEY (`id`)) 
+ENGINE = InnoDB;
+
+-- Create categories table
+CREATE TABLE `codeigniter-forum`.`categories` ( 
+     `id` INT NOT NULL AUTO_INCREMENT , 
+     `name` VARCHAR(100) NOT NULL , 
+     `description` VARCHAR(250) NOT NULL,
+     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+     PRIMARY KEY (`id`)) 
+ENGINE = InnoDB;
+
+-- Create sub_categories table
+CREATE TABLE `codeigniter-forum`.`sub_categories` 
+( 
+     `id` INT NOT NULL AUTO_INCREMENT , 
+     `maincategory_id` INT NOT NULL , 
+     `name` VARCHAR(250) NOT NULL , 
+     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+     PRIMARY KEY (`id`)) 
+ENGINE = InnoDB;
+
+-- Create likes table
+CREATE TABLE `codeigniter-forum`.`likes` ( 
+     `id` INT NOT NULL AUTO_INCREMENT , 
+     `user_id` INT NOT NULL , 
+     `post_id` INT NOT NULL , 
+     `like_type_id` INT NOT NULL , PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- Create like types
+CREATE TABLE `codeigniter-forum`.`like_type` 
+( 
+     `id` INT NOT NULL AUTO_INCREMENT , 
+     `type` VARCHAR(7) NOT NULL , PRIMARY KEY (`id`)) 
+ENGINE = InnoDB;
+
+-- Insert categories
+INSERT INTO `categories` (`id`, `name`, `created_at`) VALUES (NULL, 'Business', current_timestamp()), (NULL, 'Technology', current_timestamp())
