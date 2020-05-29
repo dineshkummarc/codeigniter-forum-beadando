bc8d2b6923aefd93214e8f4eb5e5f2b6c951b00f
Ferenc Besenyei <55558183+fepu08@users.noreply.github.com>
2020-05-29 13:30:14 +0200

    created dumping data for database

--- codeigniter-forum.sql
+++ codeigniter-forum.sql
@@ -34,7 +34,7 @@ CREATE TABLE `codeigniter-forum`.`categories` (
      `name` VARCHAR(100) NOT NULL , 
      `description` TEXT NOT NULL,
      `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
-     `post_image` VARCHAR 250 NOT NULL,
+     `post_image` VARCHAR(250) NOT NULL,
      PRIMARY KEY (`id`)) 
 ENGINE = InnoDB;
 
@@ -65,17 +65,138 @@ CREATE TABLE `codeigniter-forum`.`like_type`
      `type` VARCHAR(7) NOT NULL , PRIMARY KEY (`id`)) 
 ENGINE = InnoDB;
 
--- Insert categories
-INSERT INTO `categories` (`id`, `name`, `created_at`) VALUES (NULL, 'Business', current_timestamp()), (NULL, 'Technology', current_timestamp())
-
--- INSERT USERS
-INSERT INTO `users` (`id`, `username`, `first_name`, `last_name`, `email`, `password`, `register_date`) 
-VALUES ('1', 'admin', '', '', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', current_timestamp()), 
-(NULL, 'jondoe', 'jon', 'doe', 'jon@doe.com', '6934c4368e77e0a4e6008ae5c16fcd5d', current_timestamp())
+--
+-- Dumping data for table `users`
+--
+INSERT INTO `codeigniter-forum`.`users` (`id`, `username`, `first_name`, `last_name`, `email`, `password`, `register_date`) VALUES 
+('1', 'admin', '', '', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', current_timestamp()), 
+('2', 'jondoe', 'jon', 'doe', 'jon@doe.com', '6934c4368e77e0a4e6008ae5c16fcd5d', TIMESTAMPADD(DAY, 1, current_timestamp())),
+('3', 'johnsmith', 'john', 'smmith', 'john@smith.com', 'cd4388c0c62e65ac8b99e3ec49fd9409', TIMESTAMPADD(DAY, 2, current_timestamp()));
 -- #1 username: admin | password: admin
 -- #2 username: jondoe | password: jondoe
+-- #3 username: johnsmith | password: johnsmith
 
--- INSERT Subcategories
+--
+-- Dumping data for table `categories`
+--
+INSERT INTO `codeigniter-forum`.`categories` (`id`, `user_id`, `name`, `description`, `photo`, `created_at`) VALUES 
+('1', '1', 'Business', 'This is a category about business', 'noimage.jpg', current_timestamp()),
+('2', '2', 'Games', 'This is a category about games', 'images.jpg', TIMESTAMPADD(DAY, 1, current_timestamp()));
 
--- INSERT Posts
 
+--
+-- Dumping data for table `subcategories`
+--
+INSERT INTO `codeigniter-forum`.`subcategories` (`id`, `user_id`, `maincategory_id`, `name`, `created_at`) VALUES 
+('1', '2', '1', 'bisiness subcategory 1', current_timestamp()),
+('2', '3', '1', 'bisiness subcategory 2', TIMESTAMPADD(DAY, 1, current_timestamp())),
+
+('3', '1', '2', 'games subcategory 1', TIMESTAMPADD(DAY, 3, current_timestamp())),
+('4', '3', '2', 'games subcategory 2', TIMESTAMPADD(DAY, 4, current_timestamp()));
+
+--
+-- Dumping data for table `posts`
+--
+INSERT INTO `codeigniter-forum`.`posts` (`id`, `user_id`, `subcategory_id`, `parent_post_id`, `body`, `created_at`) VALUES 
+(NULL, '2', '1', '0', 'business subcategory 1, post body 1', current_timestamp()), 
+(NULL, '3', '1', '0', 'business subcategory 1, post body 2', TIMESTAMPADD(DAY, 2, current_timestamp())),
+(NULL, '1', '1', '0', 'business subcategory 1, post body 3', TIMESTAMPADD(DAY, 2, current_timestamp())),
+(NULL, '2', '1', '0', 'business subcategory 1, post body 4', TIMESTAMPADD(DAY, 4, current_timestamp())),
+(NULL, '3', '1', '0', 'business subcategory 1, post body 5', TIMESTAMPADD(DAY, 5, current_timestamp())),
+(NULL, '2', '1', '0', 'business subcategory 1, post body 6', TIMESTAMPADD(DAY, 5, current_timestamp())),
+(NULL, '3', '1', '0', 'business subcategory 1, post body 7', TIMESTAMPADD(DAY, 6, current_timestamp())),
+(NULL, '2', '1', '0', 'business subcategory 1, post body 8', TIMESTAMPADD(DAY, 7, current_timestamp())),
+
+(NULL, '2', '2', '0', 'business subcategory 2, post body 1', TIMESTAMPADD(DAY, 1, current_timestamp())), 
+(NULL, '3', '2', '0', 'business subcategory 2, post body 2', TIMESTAMPADD(DAY, 1, current_timestamp())),
+(NULL, '1', '2', '0', 'business subcategory 2, post body 3', TIMESTAMPADD(DAY, 1, current_timestamp())),
+(NULL, '2', '2', '0', 'business subcategory 2, post body 4', TIMESTAMPADD(DAY, 2, current_timestamp())),
+(NULL, '3', '2', '0', 'business subcategory 2, post body 5', TIMESTAMPADD(DAY, 2, current_timestamp())),
+(NULL, '2', '2', '0', 'business subcategory 2, post body 6', TIMESTAMPADD(DAY, 2, current_timestamp())),
+(NULL, '3', '2', '0', 'business subcategory 2, post body 7', TIMESTAMPADD(DAY, 2, current_timestamp())),
+(NULL, '2', '2', '0', 'business subcategory 2, post body 8', TIMESTAMPADD(DAY, 2, current_timestamp())),
+
+(NULL, '2', '3', '0', 'games subcategory 1, post body 1', TIMESTAMPADD(DAY, 5, current_timestamp())), 
+(NULL, '3', '3', '0', 'games subcategory 1, post body 2', TIMESTAMPADD(DAY, 6, current_timestamp())),
+(NULL, '1', '3', '0', 'games subcategory 1, post body 3', TIMESTAMPADD(DAY, 6, current_timestamp())),
+(NULL, '2', '3', '0', 'games subcategory 1, post body 4', TIMESTAMPADD(DAY, 7, current_timestamp())),
+(NULL, '3', '3', '0', 'games subcategory 1, post body 5', TIMESTAMPADD(DAY, 7, current_timestamp())),
+(NULL, '2', '3', '0', 'games subcategory 1, post body 6', TIMESTAMPADD(DAY, 8, current_timestamp())),
+(NULL, '3', '3', '0', 'games subcategory 1, post body 7', TIMESTAMPADD(DAY, 8, current_timestamp())),
+(NULL, '2', '3', '0', 'games subcategory 1, post body 8', TIMESTAMPADD(DAY, 8, current_timestamp())),
+
+(NULL, '2', '4', '0', 'games subcategory 2, post body 1', TIMESTAMPADD(DAY, 1, current_timestamp())), 
+(NULL, '3', '4', '0', 'games subcategory 2, post body 2', TIMESTAMPADD(DAY, 1, current_timestamp())),
+(NULL, '1', '4', '0', 'games subcategory 2, post body 3', TIMESTAMPADD(DAY, 1, current_timestamp())),
+(NULL, '2', '4', '0', 'games subcategory 2, post body 4', TIMESTAMPADD(DAY, 1, current_timestamp())),
+(NULL, '3', '4', '0', 'games subcategory 2, post body 5', TIMESTAMPADD(DAY, 1, current_timestamp())),
+(NULL, '2', '4', '0', 'games subcategory 2, post body 6', TIMESTAMPADD(DAY, 1, current_timestamp())),
+(NULL, '3', '4', '0', 'games subcategory 2, post body 7', TIMESTAMPADD(DAY, 2, current_timestamp())),
+(NULL, '2', '4', '0', 'games subcategory 2, post body 8', TIMESTAMPADD(DAY, 2, current_timestamp()));
+
+--
+-- Dumping data for table `like_type`
+--
+INSERT INTO `like_type` (`id`, `type`) VALUES (NULL, 'like'), (NULL, 'dislike')
+
+
+--
+-- Dumping data for table `like`
+--
+INSERT INTO `likes` (`id`, `user_id`, `post_id`, `like_type_id`) VALUES 
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
+(NULL, SELECT FLOOR(RAND()*(3-1+1))+1, SELECT FLOOR(RAND()*(32-1+1))+1, '1', SELECT FLOOR(RAND()*(2-1+1))+1),
